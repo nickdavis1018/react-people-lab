2b8edcdbc71a6b912397accc6fa0efbd77ee4763
Nicholas Davis <nickdavis1018@gmail.com>
2021-11-09 17:13:00 -0800

    completed backend

--- server.js
+++ server.js
@@ -1,109 +1,108 @@
-// Dependencies
-///////////////////////////////////
-// dotenv to get our env variables
-require("dotenv").config()
-// PULL PORT variable from .env
-const {PORT = 3000, MONGODB_URL} = process.env
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+// get .env variables
+require("dotenv").config();
+// pull PORT from .env, give default value of 3000
+// pull MONGODB_URL from .env
+const { PORT = 3000, MONGODB_URL } = process.env;
 // import express
-const express = require("express")
-// create app object
-const app = express()
+const express = require("express");
+// create application object
+const app = express();
 // import mongoose
-const mongoose = require("mongoose")
-// import middleware
-const cors = require("cors"); // cors headers
-const morgan = require("morgan") // logging
+const mongoose = require("mongoose");
+// import middlware
+const cors = require("cors");
+const morgan = require("morgan");
 
-//////////////////////////////////
-// Database Connection
-//////////////////////////////////
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
 // Establish Connection
 mongoose.connect(MONGODB_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-})
-
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 // Connection Events
 mongoose.connection
-.on("open", () => console.log("Connected to Mongo"))
-.on("close", () => console.log("Disonnected to Mongo"))
-.on("error", (error) => console.log(error))
+  .on("open", () => console.log("You are connected to mongoose"))
+  .on("close", () => console.log("You are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
 
-////////////////////////////////////////////////
-// Models
-////////////////////////////////////////////////
+///////////////////////////////
+// MODELS
+////////////////////////////////
 const PeopleSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    title: String
-})
+  name: String,
+  image: String,
+  title: String,
+});
 
-const People = mongoose.model("People", PeopleSchema)
+const People = mongoose.model("People", PeopleSchema);
 
-////////////////////////////////////////////////////
-// Middleware
-///////////////////////////////////////////////////
-app.use(cors()) // prevent cors errors
-app.use(morgan("dev")) // logging
-app.use(express.json()) // parse json bodies
-
-////////////////////////////////
-// Routes and Routers
 ///////////////////////////////
-// test route
+// MiddleWare
+////////////////////////////////
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+// create a test route
 app.get("/", (req, res) => {
-    res.send("hello world")
-})
+  res.send("hello world");
+});
 
-// Index Route - get request to /people
-// get us all the peoples
+// PEOPLE INDEX ROUTE
 app.get("/people", async (req, res) => {
-    try {
-        // send all the peoples
-        res.json(await People.find({}))
-    } catch (error) {
-        // send error
-        res.status(400).json({error})
-    }
-})
+  try {
+    // send all people
+    res.json(await People.find({}));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
 
-// destroy route - delete request to /people/:id
-// delete a specific people
-app.delete("/people/:id", async (req, res) => {
-    try {
-      res.json(await People.findByIdAndRemove(req.params.id));
-    } catch (error) {
-      res.status(400).json({ error });
-    }
-  });
-
-// update route - put request to /people/:id
-// update a specified person
-app.put("/people/:id", async (req, res) => {
-    try {
-        res.json(
-            await People.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        )
-    } catch (error){
-        res.status(400).json({error})
-    }
-})
-
-// Create Route - post request to /people
-// create a person from JSON body
+// PEOPLE CREATE ROUTE
 app.post("/people", async (req, res) => {
-    try {
-        // create a new people
-        res.json(await People.create(req.body))
-    } catch (error){
-        //send error
-        res.status(400).json({error})
-    }
-})
+  try {
+    // send all people
+    res.json(await People.create(req.body));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+// PEOPLE CREATE ROUTE
+app.put("/people/:id", async (req, res) => {
+  try {
+    // send all people
+    res.json(
+      await People.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+// PEOPLE CREATE ROUTE
+app.delete("/people/:id", async (req, res) => {
+  try {
+    // send all people
+    res.json(await People.findByIdAndRemove(req.params.id));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
 
 ///////////////////////////////
-//Server listener
+// LISTENER
 ////////////////////////////////
-app.listen(PORT, () => {
-    console.log(`listening on PORT ${PORT}`)
-})
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
